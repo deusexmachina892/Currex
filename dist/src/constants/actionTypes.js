569a9404e9e700fc539cc5c03f2f4b4aae9e1f8a
deusexmachina892 <deusexmachina892@gmail.com>
2019-02-20 03:25:18 -0800

    Production and Development Setups with scaffold code

--- dist/src/constants/actionTypes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// It's preferable to keep your action types together.
-exports.GET_CURRENCY_DATA_REQUEST = '@@currency/GET_CURRENCY_DATA_REQUEST';
-exports.GET_CURRENCY_DATA_SUCCESS = '@@currency/GET_CURRENCY_DATA_SUCCESS';
-exports.GET_CURRENCY_DATA_ERROR = '@currency/GET_CURRENCY_DATA_ERROR';
-//# sourceMappingURL=actionTypes.js.map
--- dist/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const react_dom_1 = require("react-dom");
-const App_1 = require("./containers/App");
-react_dom_1.render(React.createElement(App_1.default, null), document.querySelector('#root'));
-//# sourceMappingURL=index.js.map
