874911443c1c3553e8b8e3b3bf02739448ae4ed4
deusexmachina892 <deusexmachina892@gmail.com>
2019-02-24 13:07:15 -0800

    Production cleanup

--- src/client/reducers/exchangeRateReducer.ts
+++ src/client/reducers/exchangeRateReducer.ts
@@ -35,7 +35,6 @@ const reducer: Reducer = (state = initialState, action) => {
   }
 }
 
-
 // Instead of using default export, we use named exports. That way we can group these exports
 // inside the `index.js` folder.
 export { reducer as exchangeRateReducer };
