45ea050bf7a90fe6117606a1f36c6941ab628641
deusexmachina892 <deusexmachina892@gmail.com>
2019-02-24 07:57:48 -0800

    Fixed MongoURL config vars

--- src/server/config/dev.ts
+++
@@ -1,2 +0,0 @@
-export const DB: string = '';
-export const SESSION_SECRET = 'SessionMadeOfGold';
--- src/server/config/prod.ts
+++ src/server/config/prod.ts
@@ -1,2 +1,2 @@
 export const DB: string = process.env.DB;
-export const SESSION_SECRET = process.env.SESSION_SECRET;
+export const SESSION_SECRET: string = process.env.SESSION_SECRET;
