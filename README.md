370f1e87d59bee4a90c9c211dea19ffd356991e7
deusexmachina892 <deusexmachina892@gmail.com>
2019-02-25 10:17:54 -0800

    Optimized production build

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 ## Description
 
-This is a Personal Project
+Currex - An Airport Currency Exchange Service
 
 ## Validations
+Implemented Validations for empty and 0 values on the Home Page Modal
+Implemented Validations for empty and 0 values on the Admin Page
+
+## Extra Features Added
+
+
+## Lazy Loading of Components
+All the Routes are lazily loaded with react-loadable to improve performance
--- webpack.config.js
+++ webpack.config.js
@@ -2,22 +2,24 @@ const webpack = require('webpack');
 const path = require('path');
 const htmlWebPackPlugin = require('html-webpack-plugin');
 const cleanWebPackPlugin = require('clean-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const safePostCssParser = require('postcss-safe-parser');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 
 const APP_DIR  = path.resolve(__dirname, 'src', 'index.tsx');
 const BUILD_DIR = path.join(__dirname, 'dist', 'client');
 
+
 let plugins = [
-    new webpack.EnvironmentPlugin({
-        NODE_ENV: process.env.NODE_ENV || 'development', // use 'development' unless process.env.NODE_ENV is defined
-        DEBUG: 'false'
-      }),
     new htmlWebPackPlugin({
-        template: path.resolve(__dirname, 'public', 'index.html')
+        template: path.resolve(__dirname, 'public', 'index.html'),
+        favicon: path.resolve(__dirname, 'public', 'favicon.ico')
     }),
     new cleanWebPackPlugin(BUILD_DIR),
+   
    // new webpack.optimize.ModuleConcatenationPlugin(),
    // new BundleAnalyzerPlugin()
 ];
@@ -25,12 +27,64 @@ let plugins = [
 if(process.env.NODE_ENV === 'production'){
     plugins.push(
         new webpack.HashedModuleIdsPlugin(),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: 'static/css/[name].[contenthash:8].css',
+            chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+          })
     )
 } else {
     plugins.push(
         new webpack.HotModuleReplacementPlugin(),
     )
 }
+
+  // common function to get style loaders
+  const getStyleLoaders = (cssOptions, preProcessor) => {
+    const loaders = [
+      process.env.NODE_ENV === 'development' && require.resolve('style-loader'),
+     process.env.NODE_ENV === 'production' && {
+        loader: MiniCssExtractPlugin.loader,
+        options: Object.assign(
+          {}
+        ),
+      },
+      {
+        loader: require.resolve('css-loader'),
+        options: cssOptions,
+      },
+      {
+        // Options for PostCSS as we reference these options twice
+        // Adds vendor prefixing based on your specified browser support in
+        // package.json
+        loader: require.resolve('postcss-loader'),
+        options: {
+          // Necessary for external CSS imports to work
+          ident: 'postcss',
+          plugins: () => [
+            require('postcss-flexbugs-fixes'),
+            require('postcss-preset-env')({
+              autoprefixer: {
+                flexbox: 'no-2009',
+              },
+              stage: 3,
+            }),
+          ],
+          sourceMap: process.env.NODE_ENV === 'production' ? false : true,
+        },
+      },
+    ].filter(Boolean);
+    if (preProcessor) {
+      loaders.push({
+        loader: require.resolve(preProcessor),
+        options: {
+          sourceMap: process.env.NODE_ENV === 'production' ? false : true,
+        },
+      });
+    }
+    return loaders;
+  };
 module.exports =  {
     mode: process.env.NODE_ENV,
     devtool: 'source-map',
@@ -42,8 +96,8 @@ module.exports =  {
         ],
     output: {
         path: BUILD_DIR,
-        filename: process.env.NODE_ENV === 'production'?'[name].[contenthash].js':'[name].[hash].js',
-        chunkFilename: '[name].[contenthash].js',
+        filename: process.env.NODE_ENV === 'production'?'[name].[contenthash:8].js':'[name].[hash].js',
+        chunkFilename: '[name].[contenthash:8].js',
         publicPath: '/'
     },
     resolve:{
@@ -59,9 +113,19 @@ module.exports =  {
                 }
             },
             {
-                test: /\.css/,
-                use: ['style-loader', 'css-loader']
-            },
+                test: /\.css$/,
+                exclude: /\.module\.css$/,
+                use: getStyleLoaders({
+                  importLoaders: 1,
+                  sourceMap: process.env.NODE_ENV==='production'?false:true,
+                }),
+                // Don't consider CSS imports dead code even if the
+                // containing package claims to have no side effects.
+                // Remove this when webpack adds a warning or an error for this.
+                // See https://github.com/webpack/webpack/issues/6571
+                sideEffects: true,
+              },
+              // 
             {
                 test: /\.(a?png|woff|woff2|gif|eot|ttf|svg|jpe?g)$/,
                 loader: 'file-loader',
@@ -89,19 +153,59 @@ module.exports =  {
             }
           }
         },
-        minimizer: [
-            // we specify a custom UglifyJsPlugin here to get source maps in production
-        //     new UglifyJsPlugin({
-        //       cache: true,
-        //       parallel: true,
-        //       uglifyOptions: {
-        //         compress: false,
-        //         ecma: 6,
-        //         mangle: true
-        //       },
-        //       sourceMap: true
-        //     })
-        ]
+        runtimeChunk: true,
+        minimize: process.env.NODE_ENV === 'production',
+      minimizer: [
+        // This is only used in production mode
+        new TerserPlugin({
+          terserOptions: {
+            parse: {
+              // we want terser to parse ecma 8 code. However, we don't want it
+              // to apply any minfication steps that turns valid ecma 5 code
+              // into invalid ecma 5 code. This is why the 'compress' and 'output'
+              // sections only apply transformations that are ecma 5 safe
+              ecma: 8,
+            },
+            compress: {
+              ecma: 5,
+              warnings: false,
+              // Disabled because of an issue with Uglify breaking seemingly valid code:
+              comparisons: false,
+              inline: 2,
+            },
+            mangle: {
+              safari10: true,
+            },
+            output: {
+              ecma: 5,
+              comments: false,
+              ascii_only: true,
+            },
+          },
+          // Use multi-process parallel running to improve the build speed
+          // Default number of concurrent runs: os.cpus().length - 1
+          parallel: true,
+          // Enable file caching
+          cache: true,
+          sourceMap: false,
+        }),
+        // This is only used in production mode
+        new OptimizeCSSAssetsPlugin({
+          cssProcessorOptions: {
+            parser: safePostCssParser,
+            map: false
+              ? {
+                  // `inline: false` forces the sourcemap to be output into a
+                  // separate file
+                  inline: false,
+                  // `annotation: true` appends the sourceMappingURL to the end of
+                  // the css file, helping the browser find the sourcemap
+                  annotation: true,
+                }
+              : false,
+          },
+        }),
+      ],
     },
     plugins: plugins
    
